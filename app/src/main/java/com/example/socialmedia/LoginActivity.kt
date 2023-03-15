874a24d1f9413f9225d34775aef2738fe4c2b45a
kanha-jii <kanhasinghal0101@gmail.com>
2023-03-15 23:59:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/socialmedia/LoginActivity.kt
@@ -0,0 +1,71 @@
+package com.example.socialmedia
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val signupBtn = findViewById<Button>(R.id.button_signup)
+        val checkbox = findViewById<CheckBox>(R.id.login_checkbox)
+        val pass = findViewById<EditText>(R.id.login_password)
+        signupBtn.setOnClickListener() {
+            val intent  = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        val auth = FirebaseAuth.getInstance()
+        val loginButton= findViewById<Button>(R.id.button_login)
+        val emailEditText = findViewById<EditText>(R.id.login_email)
+        val passwordEditText = findViewById<EditText>(R.id.login_password)
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Login successful, go to main activity
+                            val intent = Intent(this, HomeActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            // Login failed, display an error message
+                            Toast.makeText(this, "Login failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                // Fields are empty, display an error message
+                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+            }
+        }
+        checkbox.setOnClickListener() {
+            if(checkbox.isChecked) {
+                pass.inputType = 1
+            }
+            else {
+                pass.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
+        }
+
+    }
+
+//    override fun onStart() {
+//        super.onStart()
+//        if(FirebaseAuth.getInstance().currentUser != null) {
+//            val intent = Intent(this,HomeActivity::class.java)
+//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+//            startActivity(intent)
+//            finish()
+//        }
+//    }
+}
---
+++ app/src/main/java/com/example/socialmedia/RegisterActivity.kt
@@ -0,0 +1,96 @@
+package com.example.socialmedia
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email:EditText
+    private lateinit var pass:EditText
+    private lateinit var confirmPass:EditText
+    private lateinit var progressbar:ProgressBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        email = findViewById<EditText>(R.id.register_email)
+        pass = findViewById<EditText>(R.id.register_password)
+        confirmPass = findViewById<EditText>(R.id.register_confirmpassword)
+        val checkbox = findViewById<CheckBox>(R.id.register_checkbox)
+        val loginBtn = findViewById<Button>(R.id.register_to_login)
+        val registerBtn = findViewById<Button>(R.id.button_register)
+        progressbar = findViewById<ProgressBar>(R.id.progressbar_register)
+//        Toast.makeText(this,"register btn clicked",Toast.LENGTH_LONG).show()
+        registerBtn.setOnClickListener {
+//            Toast.makeText(this,"register btn clicked",Toast.LENGTH_LONG).show()
+            progressbar.visibility = View.VISIBLE
+            createAccount()
+        }
+        checkbox.setOnClickListener() {
+            if(checkbox.isChecked) {
+                pass.inputType = 1
+                confirmPass.inputType = 1
+            }
+            else {
+                pass.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                confirmPass.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
+        }
+        loginBtn.setOnClickListener() {
+            val intent  = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun createAccount() {
+        val emailStr = email.text.toString()
+        val passStr = pass.text.toString()
+        val confirmPassStr = confirmPass.text.toString()
+        if(passStr == confirmPassStr) {
+            when {
+                TextUtils.isEmpty(emailStr) -> Toast.makeText(this, "enter email", Toast.LENGTH_LONG).show()
+                TextUtils.isEmpty(passStr) -> Toast.makeText(this, "enter password", Toast.LENGTH_LONG).show()
+                TextUtils.isEmpty(confirmPassStr) -> Toast.makeText(this, "enter confirm password", Toast.LENGTH_LONG).show()
+
+                else -> {
+                    auth = FirebaseAuth.getInstance()
+                    auth.createUserWithEmailAndPassword(emailStr, passStr).addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "successful", Toast.LENGTH_LONG).show()
+                            saveInfo(emailStr, passStr)
+                            val intent  = Intent(this,MainActivity::class.java)
+                            startActivity(intent)
+
+                        } else {
+                            val msg = task.exception!!.toString()
+                            Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+                            progressbar.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            Toast.makeText(this,"confirm password doesn't match",Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun saveInfo(emailStr : String,passStr:String) {
+        val currentUserId = FirebaseAuth.getInstance().currentUser!!.uid
+        val userRef : DatabaseReference = FirebaseDatabase.getInstance().reference.child("Users")
+
+    }
+
+
+}
