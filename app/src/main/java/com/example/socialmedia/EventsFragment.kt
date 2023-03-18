8ec233425c8167f4fd255290836f258ace1dda2f
kanha-jii <kanhasinghal0101@gmail.com>
2023-03-19 00:30:54 +0530

    storage error

--- app/src/main/java/com/example/socialmedia/EventsFragment.kt
+++ app/src/main/java/com/example/socialmedia/EventsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class EventsFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +14,14 @@ class EventsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.events_fragment,container,false)
+
+        val view = inflater.inflate(R.layout.events_fragment,container,false)
+        val recycleView = view.findViewById<RecyclerView>(R.id.recycler)
+        recycleView.layoutManager = LinearLayoutManager()
+
+
+
+        return view
     }
+
 }
